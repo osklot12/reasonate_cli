84ba74c48b4f6f29806e799212a7e36df6211773
Oskar <osklot12@gmail.com>
2024-12-21 23:25:01 +0100

    Implemented matrix scalar operator

--- tests/math/linalg/matrix_test.cpp
+++ tests/math/linalg/matrix_test.cpp
@@ -19,13 +19,25 @@ TEST(matrix_test, default_constructor) {
 TEST(matrix_test, dimension_constructor_success) {
     const size_t rows = 10;
     const size_t cols = 15;
-    Matrix<int> mat(rows, cols);
+    Matrix<int> mat{rows, cols};
 
-    int actual_rows = mat.rows();
-    int actual_cols = mat.cols();
+    size_t actual_rows = mat.rows();
+    size_t actual_cols = mat.cols();
 
     EXPECT_EQ(actual_rows, rows);
     EXPECT_EQ(actual_cols, cols);
+}
+
+// constructing matrix with custom valid dimensions and custom filler should succeed
+TEST(matrix_test, dimension_constructor_custom_fill_success) {
+    const size_t rows = 10;
+    const size_t cols = 15;
+    const double fill = 3.6;
+    Matrix<double> mat{rows, cols, fill};
+
+    double value = mat.at(3, 3);
+
+    EXPECT_EQ(value, fill);
 }
 
 // constructing matrix with invalid dimensions should throw exception
@@ -33,7 +45,7 @@ TEST(matrix_test, dimension_constructor_invalid_dimensions) {
     const size_t rows = 0;
     const size_t cols = -5;
 
-    EXPECT_THROW(Matrix<int>(rows, cols), std::invalid_argument);
+    EXPECT_THROW((Matrix<int>{rows, cols}), std::invalid_argument);
 }
 
 // constructing matrix with valid initializer list should succeed
@@ -44,7 +56,7 @@ TEST(matrix_test, initializer_list_constructor_success) {
             {7, 8, 9}
     };
 
-    Matrix<int> mat(entries);
+    Matrix<int> mat{entries};
 
     EXPECT_EQ(mat.rows(), entries.size());
     EXPECT_EQ(mat.cols(), entries.begin()->size());
@@ -84,7 +96,7 @@ TEST(matrix_test, index_operator) {
             {4, 5, 6},
             {7, 8, 9}
     };
-    Matrix<int> mat(entries);
+    Matrix<int> mat{entries};
     const int n = 2;
     const int m = 2;
 
@@ -95,7 +107,7 @@ TEST(matrix_test, index_operator) {
 
 // at function with arguments out of range should throw exception
 TEST(matrix_test, at_out_of_range) {
-    Matrix<int> mat(2, 2);
+    Matrix<int> mat{2, 2};
 
     EXPECT_THROW(mat.at(2, 2), std::out_of_range);
 }
@@ -176,14 +188,14 @@ TEST(matrix_test, plus_operator_success) {
             {5, 6},
             {7, 8}
     };
-    Matrix<int> result{
+    Matrix<int> expected{
             {6,  8},
             {10, 12}
     };
 
     mat1 = mat1 + mat2;
 
-    EXPECT_EQ(mat1, result);
+    EXPECT_EQ(mat1, expected);
 }
 
 // + operator for matrices of different dimensions should throw exception
@@ -198,4 +210,120 @@ TEST(matrix_test, plus_operator_inconsistent_matrices) {
     };
 
     EXPECT_THROW(mat1 + mat2, std::invalid_argument);
+}
+
+// square matrices multiplied together should yield expected result
+TEST(matrix_test, multiply_square_matrices_success) {
+    Matrix<double> mat1{
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+    };
+    Matrix<double> mat2{
+            {9, 8, 7},
+            {6, 5, 4},
+            {3, 2, 1}
+    };
+    Matrix<double> expected{
+            {30, 24, 18},
+            {84, 69, 54},
+            {138, 114, 90}
+    };
+
+    mat1 *= mat2;
+
+    EXPECT_EQ(mat1, expected);
+}
+
+// matrices with equal inner dimensions should give expected result
+TEST(matrix_test, multiply_nonsquare_matrices_success) {
+    Matrix<int> mat1{
+            {1, 2, 3},
+            {4, 5, 6}
+    };
+    Matrix<int> mat2{
+            {1, 2},
+            {3, 4},
+            {5, 6}
+    };
+    Matrix<int> expected{
+            {22, 28},
+            {49, 64}
+    };
+
+    auto result = mat1 * mat2;
+
+    EXPECT_EQ(result, expected);
+}
+
+// a matrix times the identity matrix should be equal to itself
+TEST(matrix_test, multiply_identity_matrix_success) {
+    Matrix<int> mat1{
+            {1, 16, 8},
+            {13, 4, 5},
+            {10, 4, -2}
+    };
+    Matrix<int> i{
+            {1, 0, 0},
+            {0, 1, 0},
+            {0, 0, 1}
+    };
+
+    auto result = mat1 * i;
+
+    EXPECT_EQ(result, mat1);
+}
+
+// *= operator using a scalar should return expected result
+TEST(matrix_test, scale_matrix_self_success) {
+    const std::initializer_list<std::initializer_list<double>> entries{
+            {1, 5},
+            {8, 3}
+    };
+    Matrix<double> mat1{entries};
+    const double scalar = 3;
+
+    mat1 *= scalar;
+
+    for (size_t i = 0; i < mat1.rows(); ++i) {
+        for (size_t j = 0; j < mat1.cols(); ++j) {
+            ASSERT_EQ(mat1.at(i, j), entries.begin()[i].begin()[j] * scalar);
+        }
+    }
+}
+
+// * operator using a scalar should return expected result
+TEST(matrix_test, scale_matrix_success) {
+    const std::initializer_list<std::initializer_list<double>> entries{
+            {17.5, 5.07},
+            {8.7, 3.13}
+    };
+    Matrix<double> mat1{entries};
+    const double scalar = 3;
+
+    const auto result = mat1 * scalar;
+
+    for (size_t i = 0; i < result.rows(); ++i) {
+        for (size_t j = 0; j < result.cols(); ++j) {
+            ASSERT_EQ(result.at(i, j), entries.begin()[i].begin()[j] * scalar);
+        }
+    }
+}
+
+// * operator using a scalar in reverse order should return expected result
+TEST(matrix_test, scale_matrix_reverse_order_success) {
+    const std::initializer_list<std::initializer_list<double>> entries{
+            {17.5, 5.07},
+            {8.7, 3.13}
+    };
+    Matrix<double> mat1{entries};
+    const double scalar = 3;
+
+    const auto result = scalar * mat1;
+
+    for (size_t i = 0; i < result.rows(); ++i) {
+        for (size_t j = 0; j < result.cols(); ++j) {
+            ASSERT_EQ(result.at(i, j), entries.begin()[i].begin()[j] * scalar);
+        }
+    }
 }
